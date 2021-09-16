b12ea1649edba83e3d42e3160db23b01fdd858af
shelik <shelkoplias@gmail.com>
2021-09-16 16:25:23 +0300

    first commit

---
+++ JWTAuth/Auth/Models/User.cs
@@ -0,0 +1,9 @@
+namespace Auth.Models
+{
+  public class User
+  {
+      public string Login { get; set; }
+      public string Password { get; set; }
+      public string Role { get; set; }
+  }
+}
